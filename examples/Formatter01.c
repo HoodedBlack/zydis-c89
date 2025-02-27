b8827bd4ff43b2c20000e655c6e6d8b657f447bc
HoodedBlack <josephdussault718@gmail.com>
2025-02-27 13:51:25 -0500

    Update Formatter01.c
    changed c99 style to c89

--- examples/Formatter01.c
+++ examples/Formatter01.c
@@ -81,7 +81,8 @@ static ZyanStatus ZydisFormatterPrintAddressAbsolute(const ZydisFormatter* forma
     ZYAN_CHECK(ZydisCalcAbsoluteAddress(context->instruction, context->operand,
         context->runtime_address, &address));
 
-    for (ZyanUSize i = 0; i < ZYAN_ARRAY_LENGTH(SYMBOL_TABLE); ++i)
+    ZyanUSize i;
+    for (i = 0; i < ZYAN_ARRAY_LENGTH(SYMBOL_TABLE); ++i)
     {
         if (SYMBOL_TABLE[i].address == address)
         {
@@ -106,8 +107,8 @@ static void DisassembleBuffer(ZydisDecoder* decoder, ZyanU8* data, ZyanUSize len
     ZydisFormatterSetProperty(&formatter, ZYDIS_FORMATTER_PROP_FORCE_SEGMENT, ZYAN_TRUE);
     ZydisFormatterSetProperty(&formatter, ZYDIS_FORMATTER_PROP_FORCE_SIZE, ZYAN_TRUE);
 
-    // Replace the `ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_ABS` function that formats the absolute
-    // addresses
+    /* Replace the `ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_ABS` function that formats the absolute */
+    /* addresses */
     default_print_address_absolute = (ZydisFormatterFunc)&ZydisFormatterPrintAddressAbsolute;
     ZydisFormatterSetHook(&formatter, ZYDIS_FORMATTER_FUNC_PRINT_ADDRESS_ABS,
         (const void**)&default_print_address_absolute);
@@ -122,8 +123,8 @@ static void DisassembleBuffer(ZydisDecoder* decoder, ZyanU8* data, ZyanUSize len
     {
         ZYAN_PRINTF("%016" PRIX64 "  ", runtime_address);
 
-        // We have to pass a `runtime_address` different to `ZYDIS_RUNTIME_ADDRESS_NONE` to
-        // enable printing of absolute addresses
+        /* We have to pass a `runtime_address` different to `ZYDIS_RUNTIME_ADDRESS_NONE` to */
+        /* enable printing of absolute addresses */
         ZydisFormatterFormatInstruction(&formatter, &instruction, operands,
             instruction.operand_count_visible, &buffer[0], sizeof(buffer), runtime_address,
             ZYAN_NULL);
@@ -149,12 +150,12 @@ int main(void)
 
     ZyanU8 data[] =
     {
-        0x48, 0x8B, 0x05, 0x39, 0x00, 0x13, 0x00, // mov rax, qword ptr ds:[<SomeModule.SomeData>]
-        0x50,                                     // push rax
-        0xFF, 0x15, 0xF2, 0x10, 0x00, 0x00,       // call qword ptr ds:[<SomeModule.SomeFunction>]
-        0x85, 0xC0,                               // test eax, eax
-        0x0F, 0x84, 0x00, 0x00, 0x00, 0x00,       // jz 0x007FFFFFFF400016
-        0xE9, 0xE5, 0x0F, 0x00, 0x00              // jmp <SomeModule.EntryPoint>
+        0x48, 0x8B, 0x05, 0x39, 0x00, 0x13, 0x00, /* mov rax, qword ptr ds:[<SomeModule.SomeData>] */
+        0x50,                                     /* push rax */
+        0xFF, 0x15, 0xF2, 0x10, 0x00, 0x00,       /* call qword ptr ds:[<SomeModule.SomeFunction>] */
+        0x85, 0xC0,                               /* test eax, eax */
+        0x0F, 0x84, 0x00, 0x00, 0x00, 0x00,       /* jz 0x007FFFFFFF400016 */
+        0xE9, 0xE5, 0x0F, 0x00, 0x00              /* jmp <SomeModule.EntryPoint> */
     };
 
     ZydisDecoder decoder;
